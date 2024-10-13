00004b4f54848d389486ef60d4028840b40697eb
Nato Boram <nato.boram+git@gmail.com>
2024-10-13 02:23:06 -0400

    💚 Put token inline

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,2 +1,7 @@
 export * from "./app.ts"
+export * from "./env.ts"
 export type * from "./express/index.ts"
+export * from "./handlers/index.ts"
+export type * from "./interfaces/index.ts"
+export * from "./middlewares/index.ts"
+export * from "./models/index.ts"
