aebe2831b85f081446d6cd6aac4f5ba807dc45fb
Nato Boram <nato.boram+git@gmail.com>
2026-02-25 19:26:45 -0500

    ♻️ Refactor URL handling

--- packages/client/src/gigachads_server_client.ts
+++ packages/client/src/gigachads_server_client.ts
@@ -12,32 +12,33 @@ import type {
 	PutTodoResponse,
 	TodoParams,
 } from "./interfaces/todo.ts"
+import { newUrl } from "./new_url.ts"
 
 export class GigachadsServerClient {
-	private readonly base: URL
-	private readonly fetch: GlobalFetch
-	private readonly token: string
+	readonly #base: URL
+	readonly #fetch: GlobalFetch
+	readonly #token: string
 
 	constructor(base: URL, fetch: GlobalFetch, token: string) {
-		this.base = base
-		this.fetch = fetch
-		this.token = token
+		this.#base = base
+		this.#fetch = fetch
+		this.#token = token
 	}
 
 	async deleteTodo(
 		id: TodoParams["id"],
 	): Promise<Response<DeleteTodoResponse>> {
-		const url = this.gcUrl(`/todos/${id}`)
+		const url = newUrl(`/todos/${id}`, this.#base)
 		return this.delete(url)
 	}
 
 	async getTodo(id: TodoParams["id"]): Promise<Response<GetTodoResponse>> {
-		const url = this.gcUrl(`/todos/${id}`)
+		const url = newUrl(`/todos/${id}`, this.#base)
 		return this.get(url)
 	}
 
 	async getTodos(query: GetTodosQuery): Promise<Response<GetTodosResponse>> {
-		const url = this.gcUrl("/todos", { ...query })
+		const url = newUrl("/todos", this.#base, { ...query })
 		return this.get(url)
 	}
 
@@ -45,12 +46,12 @@ export class GigachadsServerClient {
 		id: TodoParams["id"],
 		body: PatchTodoBody,
 	): Promise<Response<PatchTodoResponse>> {
-		const url = this.gcUrl(`/todos/${id}`)
+		const url = newUrl(`/todos/${id}`, this.#base)
 		return this.patch(url, body)
 	}
 
 	async postTodo(body: PostTodoBody): Promise<Response<PostTodoResponse>> {
-		const url = this.gcUrl("/todos")
+		const url = newUrl("/todos", this.#base)
 		return this.post(url, body)
 	}
 
@@ -58,7 +59,7 @@ export class GigachadsServerClient {
 		id: TodoParams["id"],
 		body: PutTodoBody,
 	): Promise<Response<PutTodoResponse>> {
-		const url = this.gcUrl(`/todos/${id}`)
+		const url = newUrl(`/todos/${id}`, this.#base)
 		return this.put(url, body)
 	}
 
@@ -66,60 +67,37 @@ export class GigachadsServerClient {
 		input: URL,
 		headers: HeadersInit = {},
 	): Promise<Response<T, E>> {
-		return this.fetch(input, {
+		return this.#fetch(input, {
 			headers: this.deleteHeaders(headers),
 			method: "DELETE",
 		})
 	}
 
-	private deleteHeaders(init?: HeadersInit) {
+	private deleteHeaders(init?: HeadersInit): Headers {
 		const headers = new Headers(init)
 
 		if (!headers.has("Authorization"))
-			headers.set("Authorization", `Bearer ${this.token}`)
+			headers.set("Authorization", `Bearer ${this.#token}`)
 
 		return headers
-	}
-
-	/** Creates a full URL to be used to make API requests.
-	 *
-	 * Before creating the final URL, leading slashes are removed from the path
-	 * and a trailing slash is added to the base.
-	 */
-	private gcUrl(path: string, query?: Record<string, unknown>): URL {
-		const url = new URL(
-			// The path should not start with a slash
-			path.replace(/^\/+/, ""),
-			// The base should end with a slash
-			this.base.toString().replace(/([^/]$)/, "$1/"),
-		)
-
-		if (query)
-			for (const [name, value] of Object.entries(query)) {
-				if (value instanceof Date)
-					url.searchParams.set(name, value.toISOString())
-				else url.searchParams.set(name, String(value))
-			}
-
-		return url
 	}
 
 	private async get<T, E = unknown>(
 		input: URL,
 		headers: HeadersInit = {},
 	): Promise<Response<T, E>> {
-		return this.fetch(input, {
+		return this.#fetch(input, {
 			headers: this.getHeaders(headers),
 			method: "GET",
 		})
 	}
 
-	private getHeaders(init?: HeadersInit) {
+	private getHeaders(init?: HeadersInit): Headers {
 		const headers = new Headers(init)
 
 		if (!headers.has("Accept")) headers.set("Accept", "application/json")
 		if (!headers.has("Authorization"))
-			headers.set("Authorization", `Bearer ${this.token}`)
+			headers.set("Authorization", `Bearer ${this.#token}`)
 
 		return headers
 	}
@@ -129,19 +107,19 @@ export class GigachadsServerClient {
 		body: unknown,
 		headers: HeadersInit = {},
 	): Promise<Response<T, E>> {
-		return this.fetch(input, {
+		return this.#fetch(input, {
 			headers: this.patchHeaders(headers),
 			method: "PATCH",
 			body: JSON.stringify(body),
 		})
 	}
 
-	private patchHeaders(init?: HeadersInit) {
+	private patchHeaders(init?: HeadersInit): Headers {
 		const headers = new Headers(init)
 
 		if (!headers.has("Accept")) headers.set("Accept", "application/json")
 		if (!headers.has("Authorization"))
-			headers.set("Authorization", `Bearer ${this.token}`)
+			headers.set("Authorization", `Bearer ${this.#token}`)
 		if (!headers.has("Content-Type"))
 			headers.set("Content-Type", "application/json")
 
@@ -153,19 +131,19 @@ export class GigachadsServerClient {
 		body: unknown,
 		headers: HeadersInit = {},
 	): Promise<Response<T, E>> {
-		return this.fetch(input, {
+		return this.#fetch(input, {
 			headers: this.postHeaders(headers),
 			method: "POST",
 			body: JSON.stringify(body),
 		})
 	}
 
-	private postHeaders(init?: HeadersInit) {
+	private postHeaders(init?: HeadersInit): Headers {
 		const headers = new Headers(init)
 
 		if (!headers.has("Accept")) headers.set("Accept", "application/json")
 		if (!headers.has("Authorization"))
-			headers.set("Authorization", `Bearer ${this.token}`)
+			headers.set("Authorization", `Bearer ${this.#token}`)
 		if (!headers.has("Content-Type"))
 			headers.set("Content-Type", "application/json")
 
@@ -177,19 +155,19 @@ export class GigachadsServerClient {
 		body: unknown,
 		headers: HeadersInit = {},
 	): Promise<Response<T, E>> {
-		return this.fetch(input, {
+		return this.#fetch(input, {
 			headers: this.putHeaders(headers),
 			method: "PUT",
 			body: JSON.stringify(body),
 		})
 	}
 
-	private putHeaders(init?: HeadersInit) {
+	private putHeaders(init?: HeadersInit): Headers {
 		const headers = new Headers(init)
 
 		if (!headers.has("Accept")) headers.set("Accept", "application/json")
 		if (!headers.has("Authorization"))
-			headers.set("Authorization", `Bearer ${this.token}`)
+			headers.set("Authorization", `Bearer ${this.#token}`)
 		if (!headers.has("Content-Type"))
 			headers.set("Content-Type", "application/json")
 
